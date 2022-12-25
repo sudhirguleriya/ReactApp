f831ada58fc7c73e6676b84a0147da19cd4319c3
SudhirGuleriya <sudhir.guleriya@gmail.com>
2022-12-25 18:16:59 +0530

    myfirst react app

--- src/index.js
+++ src/index.js
@@ -4,10 +4,27 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+class Clock extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {date: new Date()};
+  }
+
+  render() {
+    return (
+      <div>
+        <h1>Hello, world!</h1>
+        <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+      </div>
+    );
+  }
+}
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Clock/>
+     <App />
   </React.StrictMode>
 );
 
